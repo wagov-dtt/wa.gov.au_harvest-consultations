184076406c33a9a39badbdc1ffdee0772797010d
Adon Metcalfe <adonm@fastmail.fm>
2024-08-31 04:20:32 +0000

    handle json better, load env from codespace

--- common.py
+++ common.py
@@ -1,5 +1,5 @@
 import os
 import yaml
 
-secrets = yaml.safe_load(os.environ.get("secrets.yaml")
+secrets = yaml.safe_load(os.environ.get("SECRETS_YAML")
                          or open("secrets.yaml").read())
