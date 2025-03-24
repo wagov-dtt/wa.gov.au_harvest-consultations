6e38bccd0bf2767ab0a105567a08a1b1c36f1798
Adon Metcalfe <adon.metcalfe@dpc.wa.gov.au>
2025-03-24 06:48:52 +0000

    added a couple of new endpoint mappings

--- models/citizenspace_view.sql
+++ models/citizenspace_view.sql
@@ -15,6 +15,10 @@ SELECT
     THEN 'Department of Water and Environmental Regulation'
     WHEN url ILIKE 'https://consultation.dmirs.wa.gov.au/%'
     THEN 'Department of Energy, Mines, Industry Regulation and Safety'
+    WHEN url ILIKE 'https://consultation.infrastructure.wa.gov.au/%'
+    THEN 'Infrastructure Western Australia'
+    WHEN url ILIKE 'https://consultation.epa.wa.gov.au/%'
+    THEN 'Environmental Protection Authority'        
     ELSE department
   END AS agency,
   NULL AS tags,
